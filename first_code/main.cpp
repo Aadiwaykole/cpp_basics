dccca2c0fb944798e2319826d5e110b54794bd80
aditya <aditya@gmail.com>
2026-03-02 11:33:31 +0530

    added if else file

---
+++ first_code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Aditya!" << endl;
+    cout << "Welcome to C++ programming.";
+    return 0;
+}
---
+++ third/if.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int number;
+    // cout << "enter your first number";
+    // cin >> number;
+
+    // if(number%2 ==0){
+    //     cout << "the number is even";
+    // }
+    // else{
+    //     cout << "the number is odd";    
+    // }
+
+    // check adult or not
+
+    //  int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if (age >=18){
+    //     cout << " you are an adult ";
+    // }
+    // else{
+    //     cout <<" you are not an adult ";
+    // };
+
+
+    /*
+    // check if a number is positive, negative or zero
+    */
+//    int num ;
+//     cout << "Enter a number:";
+//     cin >> num;
+
+//     if(num >0){
+//         cout <<"The number is positive ";
+
+//     }
+//     else if (num ==0){
+//         cout <<" the number is zero";
+
+//     }
+//     else{
+//         cout <<" the number is negative";
+//     }
+
+    /*
+    check vowel or consonent 
+    */
+
+    // int c;
+    // cout << "Enter a alphabet =";
+    // cin >> c;
+
+    // if (c =='a'){
+    //     cout << "the alphabet is vowel";
+    // }
+    // else if (c =='e'){
+    //     cout << "the alphabet is vowel";
+    // }
+    // else if (c =='i'){
+    //     cout << "the alphabet is vowel";
+    // }
+    // else if (c =='o'){
+    //     cout << "the alphabet is vowel";
+    // }
+    // else if (c =='u'){
+    //     cout << "the alphabet is vowel";
+    // }
+    // else{
+    //     cout <<" the alphabet is consonent";
+    // }
+
+
+    int num ;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num ==1){
+        cout << "monday";
+    }
+    else if (num ==2){
+        cout << "tuesday";
+    }
+    else if (num ==3){
+        cout << "wednesday";
+    }
+    else if (num ==4){
+        cout << "thursday";
+    }
+    else if (num ==5){
+        cout << "friday";
+    }
+    else if (num ==6){
+        cout << "saturday";
+    }
+    else if (num ==7){
+        cout << "sunday";
+    }
+    else{
+        cout << "invalid number";
+    }
+}
