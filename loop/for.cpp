e7c90572b42548ab4876c8d5d16f082f83c3117a
aditya <aditya@gmail.com>
2026-03-02 20:15:07 +0530

    added loop file

---
+++ loop/for.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // for(int count= 0; count<=4; count++){
+    //     cout <<"coderarmy" << endl;
+    // }
+   
+
+    /*
+    n natural number print 1 to n
+    */
+    // int n ;
+    // cout << "Enter a number: ";
+    // cin>>n;
+
+    // for(int count = 1; count <= n; count++){
+    //     cout << count <<"the square is : "<< count*count << endl;
+    // }
+ 
+    // int a;
+    // cout << "Enter a number: ";
+    // cin >> a;
+
+    // for(int i=2; i<=a; i=i+2){
+    //     cout <<i<<endl;
+    // }
+
+
+  
+//     char name;
+
+//     for (name='a'; name<='z'; name++){
+//         cout << name << endl;
+//     }
+
+
+    // int n= 10;
+    
+    // for(int i = n; i>=1; i= i-2){
+    //     cout << i << endl;
+    // };
+
+
+//table draw 
+    // int n=6;
+
+    // for(int i = n; i<=60; i = i+6){
+    //     cout << i << endl;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+
+    // for(int i= 1; i<=10; i++){
+    //     cout << n << " * " << i << " = " << n*i << endl;
+    // }
+
+     
+    //square the number
+    // int n, paw, num,i;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // cout << "Enter power: ";
+    // cin >> paw;
+
+    // num = n;
+
+    // for(i=1; i<paw; i++){
+    //     num = num * n;
+    // }
+
+    // cout << num << endl;
+
+
+    //sum of numbers
+
+    // int n,i, sum=0;
+    // cout << "Enter a number: ";
+    // cin >> n;
+
+    // for(i= 1; i <=n;i++){
+    //     sum = sum + i;
+    // }
+    // cout << "The sum is: " << sum << endl;
+
+
+
+    //factorial of a number
+
+    int n, i, fact=1;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (i=1; i<= n; i++){
+        fact = fact * i;
+    }
+    cout << "The factorial is: " << fact << endl;
+ 
+    
+ }
---
+++ loop/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+    // int n, paw, num,i;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // cout << "Enter power: ";
+    // cin >> paw;
+
+    // num = n;
+
+    // for(i=1; i<paw; i++){
+    //     num = num * n;
+    // }
+
+    // cout << num << endl;
