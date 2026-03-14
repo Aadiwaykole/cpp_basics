2464c740b27bdb292c0f21a22cbfa1110c6648a6
aadiwaykole <adityawaykole494@gmail.com>
2026-03-14 09:19:42 +0530

    second same pattern follow

--- pattern/two.cpp
+++ pattern/two.cpp
@@ -8,19 +8,30 @@ int main(){
     cin >> n;
     
 
-    for (row=1; row<=n; row++){
+    // for (row=1; row<=n; row++){
 
-        //space print 
+    //     //space print 
 
-        for (col=1; col<=n-row; col++)
-            cout <<"  ";
+    //     for (col=1; col<=n-row; col++)
+    //         cout <<"  ";
 
-        //star print 
+    //     //star print 
 
-        for (col=1; col<=row; col++){  
-            cout <<"* ";
-         }
+    //     for (col=1; col<=row; col++){  
+    //         cout <<"* ";
+    //      }
+    //     cout << endl;
+
+    // }
+
+    for (row=1; row<=5; row++){
+        for (col=1; col<=5-row;col++)
+        cout << "  ";
+
+        for(col=1;col<=row; col++){
+            cout << row<<" ";
+
+        }
         cout << endl;
-
     }
 }
