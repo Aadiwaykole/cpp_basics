f885f9333fe5e709559cf5a34dd5f7d8ddd91a18
aadiwaykole <adityawaykole494@gmail.com>
2026-03-16 23:25:20 +0530

    operation file aded

---
+++ operations/first.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+using namespace std;
+
+int main (){
+
+    int a,b,c;
+
+    cout <<"Enter the value of a :";
+
+    cin >> a ;
+
+    cout <<"enter the value of b";
+
+    cin >> b;
+
+    cout<<"enter the valeu of c";
+    cin>> c;
+
+
+    // ==
+
+    // if(a==b){
+    //     cout<< "yess";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+
+
+    //>,<
+
+    // if(a>b){
+
+    //     cout<< "yess";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+
+
+    //logical oprator
+
+    if(a>b && a>c){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+
+
+}
