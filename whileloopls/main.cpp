26f394fb7ff80090a8589ae36ce087d8b8d4221d
aadiwaykole <adityawaykole494@gmail.com>
2026-03-17 22:26:31 +0530

    git while loop added

---
+++ whileloopls/main.cpp
@@ -0,0 +1,11 @@
+char name;
+    cout << "enter the alphabet:";
+    cin >> name ;
+
+    if (name="a" || name ="e" || name= "i" || name ="o" || name="u"){
+
+        cout << "this is the consonent ";
+
+
+    }else{
+        cout <<"this is not consonents";
