36c35005f2720c11a343f4944259e7825f1e391f
aadiwaykole <adityawaykole494@gmail.com>
2026-03-24 11:31:28 +0530

    prime file aded successfully

---
+++ functions/prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+bool prime(int n ){
+    if (n<2){
+        return 0;
+    };
+
+    for(int i =2 ; i<n ; i ++){
+
+        if (n%i == 0)
+        return 0;
+    };
+
+    return 1; 
+
+};
+
+int main(){
+
+    int a,b;
+
+    cout << "enter the value of a and b";
+    cin>> a,b;
+
+    cout << prime(a)<< endl;
+}
