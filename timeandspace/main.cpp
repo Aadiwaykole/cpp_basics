ead9d02fb336cc04dbda88eacf2263edba499a9f
aadiwaykole <adityawaykole494@gmail.com>
2026-03-27 11:31:38 +0530

    added recursion file

--- timeandspace/main.cpp
+++ timeandspace/main.cpp
@@ -3,18 +3,26 @@ using namespace std;
 
 int main (){
 
-    int i , j , n ;
+    int i , j , n,k ;
 
     cout << " enter the value of n : ";
 
     cin>> n ; 
 
-    for(i =1 ; i<= n ; i ++){
+    // for(i =1 ; i<= n ; i ++){
 
-        for (j=1; j<=n; j++){
-            cout <<"chamka";
-        }
+    //     for (j=1; j<=i; j++){
+    //         cout <<"chamka";
+    //     }
 
-        cout<< endl;
+    //     cout<< endl;
+    // }
+
+
+    for (i=1; i <=n ; i*2){
+
+        cout<< "chamka";
+
     }
+    
 }
